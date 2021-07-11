3883135975890277fc92e6f4635b9599d6382304
LukasFrankenQ <55196140+LukasFrankenQ@users.noreply.github.com>
2021-07-11 22:00:00 +0200

    added skeleton

---
+++ src/test.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+from utils import *
+
+def test():
+    raise NotImplementedError("implement me!")
+  
+def main():
+    raise NotImplementedError("implement me!")
+
+
+if __name__ == "__main__":
+  main()
