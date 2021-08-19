59488f08dd6c899cdd6e6e4a5d7013ce6bf2450c
mnm-matin <matin.mahmood99@gmail.com>
2021-08-19 16:57:48 +0100

    fix imports, replace d2l with matplotlib

--- image_download/vis.py
+++ image_download/vis.py
@@ -1,5 +1,8 @@
 %matplotlib inline
-from d2l import tensorflow as d2l
+import matplotlib.pyplot as plt
+import matplotlib
+import geopandas as gpd
+import numpy as np
 
 def vis_image(examples, path, show_box=True, idx=0):
     """
@@ -25,27 +28,28 @@ def vis_image(examples, path, show_box=True, idx=0):
     Returns:
 
     -
-    """
+    """ 
 
     if isinstance(examples, str): examples = gpd.read_file(path + examples)
 
     example = examples.iloc[idx]
-
-    d2l.set_figsize(figsize=(10, 7))
-    img = d2l.plt.imread(path + example.filename)
-    fig = d2l.plt.imshow(img)
+    plt.figure(figsize=(10, 7))
+    img = plt.imread(path + example.filename)
+    fig = plt.imshow(img)
 
     if show_box:
         bbox = [example.ul_x, example.ul_y, example.lr_x, example.lr_y]
-        rect = d2l.plt.Rectangle(xy=(bbox[0], bbox[1]), width=bbox[2] - bbox[0],
+        rect = plt.Rectangle(xy=(bbox[0], bbox[1]), width=bbox[2] - bbox[0],
                              height=bbox[3] - bbox[1], fill=False,
                              edgecolor="r", linewidth=2)
         fig.axes.add_patch(rect)
     
-    d2l.plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
     idx = np.random.randint(0, 20)
+    # vis_image("tower_examples.geojson", "examples/", show_box=False, idx=idx)
+    # vis_image("tower_examples.geojson", "examples/", show_box=True, idx=idx)
     vis_image("tower_examples.geojson", "./../examples/", show_box=False, idx=idx)
     vis_image("tower_examples.geojson", "./../examples/", show_box=True, idx=idx)
