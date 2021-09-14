696568593e84268ae6eaa8c61547a2b230fccbce
LukasFrankenQ <lukas.b.franken@gmail.com>
2021-09-14 12:26:39 +0100

    made function to_coco a function

--- image_download/create_coco_format.py
+++ image_download/create_coco_format.py
@@ -3,92 +3,112 @@ import os
 import json
 import geopandas as gpd
 
-#%%
-img_dir = "./examples/" # TODO: split dataset into /examples/train and /examples/val
-examples = gpd.read_file(os.path.join(img_dir, "tower_examples.geojson"))
-img_list = [img for img in os.listdir(img_dir) if img.endswith(".png")]
-images = []
-annotations = []
-for idx, exp in examples.iterrows():
 
-    # --- image ---
-    image={}
-    img_filename = str(exp.filename) + ".png"
-    img_path = os.path.join(img_dir, img_filename) # Not needed
-    
-    if img_filename not in img_list: # Checks if the png file exists
-        print("ERROR: EXAMPLE NOT FOUND")
+# TODO: split dataset into /examples/train and /examples/test
+def to_coco(img_dir, examples, height=512, width=512):
+    """
+    splits the dataframe of all examples into many json files in coco format
+    ----------
+    Arguments:
+        img_dir : (str)
+            path to image directory
+        examples : (str)
+            filename of dataframe
+        height : (int)
+            height (in pixels) of images
+        width : (int)
+            width (in pixels) of images
+    """
 
-    image_id = int(exp.filename) # TODO: rename filename to id in make_examples
-    image["id"] = image_id
-    image["width"] = 256 # TODO : Do not hardcode
-    image["height"] = 256
-    image["file_name"] = f"{image_id}.png"
-    image["license"] = 1
-    images.append(image)
+    img_list = [img for img in os.listdir(img_dir) if img.endswith(".png")]
+    images = []
+    annotations = []
+    for idx, exp in examples.iterrows():
 
-    # --- annotation ---
-    annotation = {}
+        # --- image ---
+        image={}
+        img_filename = str(exp.filename)
+        img_path = os.path.join(img_dir, img_filename) # Not needed
 
-    bbox_width = 30 # tower_width from make_examples
-    bbox_height = 25 # tower_height from make_examples
-    # TODO: Return width and height instead of "lr_x" and "lr_y" in make_examples
+        if img_filename not in img_list: # Checks if the png file exists
+            print(f"ERROR: {img_filename} NOT FOUND in {img_dir}")
 
-    x_top_left = exp.ul_x
-    y_top_left = exp.ul_y
+        image_id = int(exp.filename.split(".")[0])
+        image["id"] = image_id
+        image["width"] = width
+        image["height"] = height
+        image["file_name"] = f"{image_id}.png"
+        image["license"] = 1
+        images.append(image)
 
+        # --- annotation ---
+        annotation = {}
 
-    annotation["id"] = image_id # Just needs a unique id, we only have one annotation per image
-    annotation["image_id"] = image_id # Should be same as image["id"]
-    annotation["segmentation"] = [] # Stays like this as we are not doing segmentation
-    annotation["area"] = bbox_width * bbox_height # Easy
-    annotation["iscrowd"] = 0 # 0 as we only have one class
+        bbox_width = 30 # tower_width from make_examples
+        bbox_height = 25 # tower_height from make_examples
+        # TODO: Return width and height instead of "lr_x" and "lr_y" in make_examples
 
-    annotation["bbox"] = [x_top_left, y_top_left, bbox_width, bbox_height] # format for coco
-    annotation["category_id"] = 1 # we only have one category so hardcoded
+        x_top_left = exp.ul_x
+        y_top_left = exp.ul_y
 
-    annotations.append(annotation)
+        annotation["id"] = image_id # Just needs a unique id, we only have one annotation per image
+        annotation["image_id"] = image_id # Should be same as image["id"]
+        annotation["segmentation"] = [] # Stays like this as we are not doing segmentation
+        annotation["area"] = bbox_width * bbox_height # Easy
+        annotation["iscrowd"] = 0 # 0 as we only have one class
 
-# print(image)
-# print(annotation)
+        annotation["bbox"] = [x_top_left, y_top_left, bbox_width, bbox_height] # format for coco
+        annotation["category_id"] = 1 # we only have one category so hardcoded
 
-#%%
-info={# Purely Cosmetic
-    "year":2021,
-    "version": "1.0",
-    "description": "Satellite Imagery Dataset",
-    "contributor": "PyPSA-meets-Africa",
-    "url": "example.com",
-    "date_created": "2021/8/28"
-}
+        annotations.append(annotation)
 
-license = {# Purely Cosmetic
-    "id": "1",
-    "name": "Maxar?", 
-    "url": "maxar.org"
-}
+    # print(image)
+    # print(annotation)
 
+    #%%
+    info={# Purely Cosmetic
+        "year":2021,
+        "version": "1.0",
+        "description": "Satellite Imagery Dataset",
+        "contributor": "PyPSA-meets-Africa",
+        "url": "example.com",
+        "date_created": "2021/8/28"
+    }
 
-category = {
-    "id": 1, # id starts from 1
-    "name": "tower",
-    "supercategory": "power" # Not sure if supercategory is required
-}
-
-
-
-#%%
-coco_format = {
-    "info": info,
-    "licenses": [license],
-    "categories": [category],
-    "images": images,
-    "annotations": annotations
-}
-
-# print(coco_format)
+    license = {# Purely Cosmetic
+        "id": "1",
+        "name": "Maxar?",
+        "url": "maxar.org"
+    }
 
 
-# %%
-with open(os.path.join(img_dir,'tower_coco.json'), 'w') as fp:
-    json.dump(coco_format, fp)
+    category = {
+        "id": 1, # id starts from 1
+        "name": "tower",
+        "supercategory": "power" # Not sure if supercategory is required
+    }
+
+
+
+    #%%
+    coco_format = {
+        "info": info,
+        "licenses": [license],
+        "categories": [category],
+        "images": images,
+        "annotations": annotations
+    }
+
+    # print(coco_format)
+
+
+    # %%
+    with open(os.path.join(img_dir,'tower_coco.json'), 'w') as fp:
+        json.dump(coco_format, fp)
+
+
+
+if __name__ == "__main__":
+    img_dir = "./examples/"
+    examples = gpd.read_file(os.path.join(img_dir, "tower_examples.geojson"))
+    to_coco(img_dir, examples)
