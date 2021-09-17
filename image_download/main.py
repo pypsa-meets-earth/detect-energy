e03b19640761bd7c712f31de5c4ed7880a724ece
mnm-matin <matin.mahmood99@gmail.com>
2021-09-17 15:29:21 +0100

    refactor main to avoid colab problems

--- image_download/main.py
+++ image_download/main.py
@@ -2,13 +2,13 @@ import os
 import sys
 import geopandas as gpd
 import pandas as pd
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), 'detect_energy/image_download/')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname("__file__"), 'detect_energy/image_download/')))
 import numpy as np
 
 from make_examples import make_polygon_list, make_examples
-from filter import filter_images
+from filter_tiles import filter_images
 
-def main():
+def make_all_examples():
     print(os.getcwd())
 
     # SIERRA LEONE
@@ -35,7 +35,7 @@ def main():
     make_examples(towers_file, 
                   polygons_gh, 
                   img_path="/GH_", 
-                  max_length=np.inf)
+                  max_length=1000)
 
     # merge the respective dataframes
     sl = gpd.read_file("SL_tower_examples.geojson")
@@ -50,9 +50,14 @@ def main():
 
     print("Deleted contributing individual dataframes!")
 
-    # Filtering images for black spots or cloudyness
-    filter_images('', "tower_examples.geojson")
- 
 
 if __name__ == "__main__":
-    main()
+    make_all_examples()
+    # Filtering images for black spots or cloudyness
+    filter_images('tower_examples.geojson', delete_filtered=True)
+
+
+# def split_data(df_dir, df, train_ratio=0.8, seed=202):
+#     train=df.sample(frac=train_ratio,random_state=seed) #random state is a seed value
+#     test=df.drop(train.index)
+#     train.to_file(df_dir + "tower_train.geojson", driver="GeoJSON")
