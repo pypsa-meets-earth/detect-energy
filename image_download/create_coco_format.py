ca8c833ce16a8808860ba29c79fc5ffeb35dfe90
mnm-matin <matin.mahmood99@gmail.com>
2021-09-18 23:58:20 +0100

    general improvements

--- image_download/create_coco_format.py
+++ image_download/create_coco_format.py
@@ -4,37 +4,40 @@ import json
 import geopandas as gpd
 
 
-# TODO: split dataset into /examples/train and /examples/test
-def to_coco(img_dir, examples, height=512, width=512):
+def to_coco(gdf_path, suffix= '', height=512, width=512):
     """
     splits the dataframe of all examples into many json files in coco format
     ----------
     Arguments:
-        img_dir : (str)
-            path to image directory
-        examples : (str)
-            filename of dataframe
+        gdf_path : (str)
+            path to train/val geojson
+        suffix : (str)
+            suffix for filename (_train or _val)
         height : (int)
             height (in pixels) of images
         width : (int)
             width (in pixels) of images
     """
+    examples = gpd.read_file(gdf_path)
+    img_dir = os.path.dirname(os.path.abspath(gdf_path))
+    # fn = os.path.basename(gdf_path)
 
     img_list = [img for img in os.listdir(img_dir) if img.endswith(".png")]
+
     images = []
     annotations = []
-    for idx, exp in examples.iterrows():
+    for row in examples.itertuples():
 
         # --- image ---
         image={}
-        img_filename = str(exp.filename)
+        img_filename = str(row.filename)
         img_path = os.path.join(img_dir, img_filename) # Not needed
 
         if img_filename not in img_list: # Checks if the png file exists
             print(f"ERROR: {img_filename} NOT FOUND in {img_dir}")
 
-        image_id = int(exp.filename.split(".")[0].split("_")[1])
-        prefix = exp.filename.split("_")[0] + "_"
+        image_id = int(row.filename.split(".")[0].split("_")[1])
+        prefix = row.filename.split("_")[0] + "_"
         image["id"] = image_id
         image["width"] = width
         image["height"] = height
@@ -45,10 +48,10 @@ def to_coco(img_dir, examples, height=512, width=512):
         # --- annotation ---
         annotation = {}
 
-        x_top_left = exp.ul_x
-        y_top_left = exp.ul_y
-        bbox_width = exp.lr_x - exp.ul_x
-        bbox_height = exp.lr_y - exp.ul_y
+        x_top_left = row.ul_x
+        y_top_left = row.ul_y
+        bbox_width = row.lr_x - row.ul_x
+        bbox_height = row.lr_y - row.ul_y
 
 
         annotation["id"] = image_id # Just needs a unique id, we only have one annotation per image
@@ -101,14 +104,10 @@ def to_coco(img_dir, examples, height=512, width=512):
 
     # print(coco_format)
 
-
-    # %%
-    with open(os.path.join(img_dir,'tower_coco.json'), 'w') as fp:
+    with open(os.path.join(img_dir,'tower_coco' + suffix + '.json'), 'w') as fp:
         json.dump(coco_format, fp)
 
 
-
+# %%
 if __name__ == "__main__":
-    img_dir = "./examples/"
-    examples = gpd.read_file(os.path.join(img_dir, "tower_examples.geojson"))
-    to_coco(img_dir, examples)
+    to_coco("./examples/tower_examples.geojson")
