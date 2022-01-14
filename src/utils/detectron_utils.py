da3941f4224063fcce8c6cf042b29afe27a79386
LukasFrankenQ <lukas.b.franken@gmail.com>
2022-01-14 14:55:13 +0000

    cleaned up some detectron utils

--- src/utils/detectron_utils.py
+++ src/utils/detectron_utils.py
@@ -15,73 +15,118 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.data.datasets import register_coco_instances
 
-def build_predictor(threshold=0.2, frcnn=True, model_path=None):
 
-    print('In build predictor:')
+def register(name, path):
+    '''
+    adds a dataset to the detectron2.DatasetCatalog.
+    ! Has to be in coco-format !
+
+    Parameters
+    -----------
+    name : str
+        name of the dataset
+    path : str
+        path to dataset with  path
+                                |--data/<images>
+                                L--labels.json
+
+    Returns
+    ----------
+    -
+
+    '''
+
+    print(f'Adding dataset {name} from path \n {path} \n to \
+            DatasetCatalog and MetadataCatalog.') 
+
+    if name in DatasetCatalog.list():
+        DatasetCatalog.remove(name)
+        MetadataCatalog.remove(name)
+
+    ds_path = os.path.join(path, 'data')
+    json_path = os.path.join(path, 'labels.json')
+    register_coco_instances(name, {}, json_path, ds_path)
+
+
+def build_predictor(threshold=0.1, frcnn=True, model_path=None):
+    '''
+    Loads and returns a detectron2.DefaultPredictor with a Faster R-CNN or RetinaNet model
+
+    Parameters
+    ----------
+    threshold : float
+        detection threshold set in the network
+    frcnn : bool
+        loads frcnn if true, else retinanet
+    model_path : str
+        path to the model to load
+
+    Returns
+    ----------
+    predictor : detectron2.DefaultPredictor
+        with desired model
+
+    '''
+
+    if frcnn: 
+        print('Obtaining faster RCNN from path:')
+    else: 
+        print('Obtaining RetinaNet from path:')
     print(model_path)
 
-    ds_path = f'/content/drive/My Drive/PyPSA_Africa_images/datasets/duke_train/data/' 
-    json_path = f'/content/drive/My Drive/PyPSA_Africa_images/datasets/duke_train/labels.json'
-    ds_name = 'duke'
-    
-    if ds_name in DatasetCatalog.list():
-        DatasetCatalog.remove(ds_name)
-        MetadataCatalog.remove(ds_name)
-    
-    register_coco_instances(ds_name, {}, json_path, ds_path)
-    
-    retina_net = 'retinanet_R_101_FPN_3x.yaml'
-    frcnn = 'faster_rcnn_R_101_FPN_3x.yaml'
+    if model_path is None: 
+        print('Warning: Returned model will be untrained, as no model_path was passed!')
     
     if frcnn:
-        current = frcnn
+        current = 'faster_rcnn_R_101_FPN_3x.yaml'
+
     else:
-        current = retina_net
+        current = 'retinanet_R_101_FPN_3x.yaml'
     
     cfg = get_cfg() # Model Config
     cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/"+current))
 
     if frcnn:
-        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2   # for R-CNN Models
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold  # for R-CNN Models
-        if model_path is None:
-            model_path = os.path.join('/content', 'drive', 'MyDrive', 'PyPSA_Africa_images', 'models', 
-                                        '2021-11-29_frcnn_180000_dukeset', 'model_final.pth')
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold
 
     else:
-        cfg.MODEL.RETINANET.NUM_CLASSES = 2   # for RetinaNet  
-        cfg.MODEL.RETINANET.SCORE_THRESH_TEST = threshold  # for Retinanet
-        model_name = '2021-10-18_retina101_100000_dukeset/model_final.pth'
-        if not model_path is None:
-            model_path = '/content/drive/MyDrive/PyPSA_Africa_images/models/'+model_name
+        cfg.MODEL.RETINANET.NUM_CLASSES = 2
+        cfg.MODEL.RETINANET.SCORE_THRESH_TEST = threshold
     
     cfg.MODEL.WEIGHTS = model_path
-
-
-    print('working with path: ', model_path)
     cfg.INPUT.FORMAT = 'BGR'
 
-    predictor = DefaultPredictor(cfg)                                
-    model = build_model(cfg)
-
-    checkpointer = DetectionCheckpointer(model)
-    checkpointer.load(model_path)
-
-    return predictor, model
+    return DefaultPredictor(cfg)                                
 
 
-def eval_predictor(imgs, model_path=None, threshold=0.1):
+def eval_predictor(imgs, model_path=None, frcnn=True, threshold=0.1):
+    '''
+    Exhibits performance of model on some images
+    (Also expects images to have width and height of 256 pixels)
 
-    print('in eval pred: ')
-    print(model_path)
+    Parameters
+    ----------
+    imgs : list of height x width np.array
+        list of images. On all images inference will be performed
+    model_path : str
+        path to model weights
+    frcnn : bool
+        loads frcnn if true, else retinanet
+    threshold : float
+        detection threshold set in the network
+    
+    Returns 
+    -----------
+    -
 
-    predictor, model = build_predictor(threshold=threshold, frcnn=True, model_path=model_path)
-    model.eval()
+    '''
+
+    predictor = build_predictor(threshold=threshold, frcnn=frcnn, model_path=model_path)
 
     for img in imgs:
 
         out = predictor(img)
-        # out = predictor(img[:,:,::-1])
         v = Visualizer(img, MetadataCatalog.get('duke'), scale=1.5)
         out = v.draw_instance_predictions(out["instances"].to("cpu"))
         cv2_imshow(out.get_image())
@@ -91,10 +136,8 @@ def eval_predictor(imgs, model_path=None, threshold=0.1):
             img = predictor.aug.get_transform(img).apply_image(img)
             img = torch.as_tensor(img.astype('float32').transpose(2, 0, 1))
             x = [{'image': img, 'width': 256, 'height': 256}]
-
             pred = predictor.model(x)
-            print(pred)
-            
+            print(f"Prediction: {pred}")
 
 
 if __name__ == '__main__':
