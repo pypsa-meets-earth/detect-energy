5406245063d613b7cbe22b201bad4363bd64407e
LukasFrankenQ <lukas.b.franken@gmail.com>
2022-03-01 13:50:03 +0000

    fixed typo

--- src/train/ubteacher/ubteacher/utils/registration.py
+++ src/train/ubteacher/ubteacher/utils/registration.py
@@ -11,7 +11,7 @@ DATASETS_PATH = os.environ.get('PROJECT_DATASETS')
 
 def register_all():
     # register used datasets
-    ds_names = ['fake_maxar', 'maxar']
+    ds_names = ['fake_maxar', 'duke']
     modes = ['train', 'val']
 
     for name, mode in product(ds_names, modes):
